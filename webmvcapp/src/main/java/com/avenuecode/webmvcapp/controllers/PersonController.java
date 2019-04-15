08e374b387bf0024f808e0ad2f208af12ac49464
sudarshan <48923126+sudarshandasari@users.noreply.github.com>
2019-04-15 18:12:36 +0530

    changed the controller

--- webmvcapp/src/main/java/com/avenuecode/webmvcapp/controllers/PersonController.java
+++ webmvcapp/src/main/java/com/avenuecode/webmvcapp/controllers/PersonController.java
@@ -2,6 +2,8 @@ package com.avenuecode.webmvcapp.controllers;
 
 import com.avenuecode.webmvcapp.domain.Person;
 import com.avenuecode.webmvcapp.services.PersonService;
+import com.avenuecode.webmvcapp.services.templat.PersonServiceTemplate;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 public class PersonController {
 
     private final PersonService personService;
+    private PersonServiceTemplate personServiceTemplate;
 
     public PersonController(PersonService personService){
         this.personService = personService;
@@ -21,21 +24,39 @@ public class PersonController {
     public ResponseEntity<Person> findById(@PathVariable String id){
         return ResponseEntity.ok(this.personService.findById(id));
     }
-
+    
+    @GetMapping("/api/v1/personstemplate/{id}")
+    public ResponseEntity<Person> findById1(@PathVariable String id){
+        return ResponseEntity.ok(this.personServiceTemplate.findById(id));
+    }
+    
+  
     @GetMapping("/api/v1/persons")
     public ResponseEntity<List<Person>> findAll(){
         return ResponseEntity.ok(this.personService.findAll());
+    }
+    
+    @GetMapping("/api/v1/personstemplate")
+    public ResponseEntity<List<Person>> findAll1(){
+        return ResponseEntity.ok(this.personServiceTemplate.findAll());
     }
 
     @PostMapping("/api/v1/persons")
     public ResponseEntity<Person> save(@RequestBody Person person){
         return ResponseEntity.ok(this.personService.save(person));
     }
-
-    @PutMapping("/api/v1/persons/{id}")
-    public ResponseEntity<Person> update(@PathVariable String id, @RequestBody Person person){
-       // person.setId(id);
-        return ResponseEntity.ok(this.personService.update(person));
+    
+    
+    
+    @PostMapping("/api/v1/personstemplate")
+    public void save1(@RequestBody Person person){
+         personServiceTemplate.create(person);
+    }
+    
+      @PutMapping("/api/v1/personstemplate/{id}")
+    public void update1(@PathVariable String id, @RequestBody Person person){
+       
+        personService.update(person);
     }
 
     @DeleteMapping("/api/v1/persons/{id}")
